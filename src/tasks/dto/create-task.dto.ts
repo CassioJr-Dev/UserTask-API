bf7def137f69be5908a511f2195db3cf195758d2
Cassio <cassiojr0108@gmail.com>
2024-01-03 00:32:51 -0300

    creating data validations in post and update entries

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-  completed: boolean;
+  @IsBoolean()
+  @IsOptional()
+  completed?: boolean;
 }
 
